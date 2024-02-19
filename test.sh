9b9458d44a61e31dec8a1b03c5829d43620b003b
Andrew Cole <andrew.cole@illallangi.com>
2024-02-20 10:48:03 +1100

    Removed and ignored test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-podman build --format docker --build-arg hooks=plexes . -t registry.great-tuna.ts.net/plex-controller:latest
-podman push registry.great-tuna.ts.net/plex-controller:latest
-kubectl delete deploy -n kube-system plex-controller || true
-cat manifests/plex-controller.yaml | sed "s|ghcr.io/illallangi|registry.great-tuna.ts.net|g" | kubectl apply -f -
